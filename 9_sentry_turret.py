ba2132e78ea36f28fd015e5f9abdc52c13349168
CantStopThinking2Much <bornagainatheist19@gmail.com>
2025-03-31 20:14:31 -0400

    testing testing

---
+++ 9_sentry_turret.py
@@ -0,0 +1,28 @@
+import os
+import time
+from datetime import datetime
+from playsound3 import playsound
+import pyttsx3
+import cv2 as cv
+from path import Path
+
+# Pythonpy program that disables the sentry gun's firing mechanism when it detects human faces in an image
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 145)
+engine.setProperty('volume', 1.0)
+
+root_dir = os.path.abspath('./')
+gunfire_path = os.path.join(root_dir, 'gunfire.wav')
+tone_path = os.path.join(root_dir, 'tone.wav')
+
+# If any problems arise check the path below
+# TODO find haarcascade_frontalface_default and haarcascade_eye.xml in C:/ directory
+path = Path.path
+face_cascade = cv.CascadeClassifier(path + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier(path + 'haarcascade_eye.xml')
+
+os.chdir('Faces')
+contents = sorted(os.listdir())
+
+print("Hello world")
